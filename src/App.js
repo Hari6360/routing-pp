3aec83409a669b52d04ef68b38361e3c44ef05b4
Hari Prasad <101623030+Hari6360@users.noreply.github.com>
2022-06-03 20:12:07 +0530

    ProductPage

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./component/Home";
+import { Route, Routes } from "react-router-dom";
+import Products from "./component/Products";
+import Navbar from "./component/Navbar";
+import ProductDetails from "./component/ProductDetails";
+import { ProductProvider } from "./context/ProductsData";
+
+function App() {
+	return (
+		<div className="App">
+			<ProductProvider>
+				<Navbar />
+				<Routes>
+					<Route path="" element={<Home />} />
+					<Route path="products/*" element={<Products />}>
+						<Route path=":id" element={<ProductDetails />} />
+					</Route>
+				</Routes>
+			</ProductProvider>
+		</div>
+	);
+}
+
+export default App;
